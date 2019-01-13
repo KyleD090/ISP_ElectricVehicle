2dedd64c1e7f5fb4c3e09c2ce9fcc5942ce3f9b7
KyleD090 <36234254+KyleD090@users.noreply.github.com>
2019-01-13 15:27:34 -0500

    Delete clutch.java

--- clutch.java
+++
@@ -1,8 +0,0 @@
-public class clutch {
-    boolean heldDown;
-    clutch(){heldDown = false;}
-
-    public void pressClutch(){heldDown = true;}
-
-    public void releaseClutch(){heldDown = false;}
-}
